82f36bf9fa065a046da91fe5fca596d409921e8a
Nalin Dalal <nalindalal2004@gmail.com>
2026-06-20 22:46:48 +0530

    live server

--- lua/custom/plugins/nalin.lua
+++ lua/custom/plugins/nalin.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"barrett-ruth/live-server.nvim",
-		build = "pnpm add -g live-server",
+		build = "npm install -g live-server",
 		cmd = { "LiveServerStart", "LiveServerStop" },
 		config = true,
 	},
